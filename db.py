b90f7e7242fa5fde08f65ece3a93503c73c488e9
valkrysov <valkrysov@gmail.com>
2026-08-21 19:29:45 -0700

    "Update db.py to support Neon wake up handling after inactivity

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from sqlalchemy import create_engine
 
-
 @st.cache_resource
 def get_engine():
-
     return create_engine(
-        st.secrets["DATABASE_URL"]
+        st.secrets["DATABASE_URL"],
+        pool_pre_ping=True,
+        pool_recycle=300,
+    )
     )
